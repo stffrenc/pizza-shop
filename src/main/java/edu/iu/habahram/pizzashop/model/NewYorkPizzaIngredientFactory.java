593a7ba5d4c0e1bb1e2bc5047a1e916b3b6e9937
French II, Stephan Floyd <stffrenc@iu.edu>
2024-02-19 09:16:38 -0500

    Added New York Pizzaria

---
+++ src/main/java/edu/iu/habahram/pizzashop/model/NewYorkPizzaIngredientFactory.java
@@ -0,0 +1,31 @@
+package edu.iu.habahram.pizzashop.model;
+
+public class NewYorkPizzaIngredientFactory implements PizzaIngredientFactory{
+
+    public Dough createDough() {
+        return new ThickCrustDough();
+    }
+
+    public Sauce createSauce() {
+        return new MarinaraSauce();
+    }
+
+    public Cheese createCheese() {
+        return new ReggianoCheese();
+    }
+
+    public Veggies[] createVeggies() {
+        Veggies veggies[] = { new Mushrooms(),
+                new BlackOlives(),
+                new RedPeppers() };
+        return veggies;
+    }
+
+    public Pepperoni createPepperoni() {
+        return new SlicedPepperoni();
+    }
+
+    public Clams createClam() {
+        return new FrozenClams();
+    }
+}
---
+++ src/main/java/edu/iu/habahram/pizzashop/model/RedPeppers.java
@@ -0,0 +1,8 @@
+package edu.iu.habahram.pizzashop.model;
+
+public class RedPeppers implements Veggies{
+    @Override
+    public String toString() {
+        return "Red Peppers";
+    }
+}
